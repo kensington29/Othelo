837b6254dc5dabb888b70bac5a8e78b217a77e29
kensington29 <64568859+kensington29@users.noreply.github.com>
2020-07-23 12:20:43 +0900

    オセロゲーム
    Javascript練習用に制作
    firebase realtime databaseを使用した対戦ゲーム
    ゲームルール部分未実装

---
+++ sample.js
@@ -0,0 +1,8 @@
+var nico = new Nico({
+  ele: document.getElementById('nico'), // スクリーンになる要素
+  width: 400,                           // スクリーン幅
+  height: 400,                          // スクリーン高さ
+  font: 50,                             // フォントサイズ
+  color: '#fff',                        // フォントカラー
+  speed: 3                              // 流れるスピード
+});
